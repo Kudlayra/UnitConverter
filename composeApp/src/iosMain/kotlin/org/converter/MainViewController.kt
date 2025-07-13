fabfd8aa3abf2db7946dbb748766a06f61974e29
Ruslan <Kudlayra@gmail.com>
2025-07-13 20:23:41 +0500

    development: Запуск на IOS

--- composeApp/src/iosMain/kotlin/org/converter/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/converter/MainViewController.kt
@@ -2,5 +2,6 @@ package org.converter
 
 import androidx.compose.ui.window.ComposeUIViewController
 import org.converter.app.App
+import org.converter.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(configure = { initKoin() }) { App() }
