8dacd4533986ad4c3f75b05afcc54499bd216056
Ruslan <Kudlayra@gmail.com>
2025-07-13 21:37:22 +0500

    development: Реализация edge to edge

--- composeApp/src/commonMain/kotlin/org/converter/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/converter/app/App.kt
@@ -1,13 +1,16 @@
 package org.converter.app
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import org.converter.core.presentation.darkGreen
 import org.converter.core.presentation.focusClick
 import org.converter.units.presentation.ui.UnitsScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -17,19 +20,17 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     val navController = rememberNavController()
     MaterialTheme {
-        Scaffold(
-            modifier = Modifier.focusClick(),
-            content = { innerPadding ->
-                NavHost(
-                    modifier = Modifier.padding(innerPadding),
-                    navController = navController,
-                    startDestination = Route.UnitsScreen
-                ) {
-                    composable<Route.UnitsScreen> {
-                        UnitsScreen()
-                    }
-                }
+        NavHost(
+            modifier = Modifier
+                .background(darkGreen)
+                .windowInsetsPadding(WindowInsets.safeDrawing)
+                .focusClick(),
+            navController = navController,
+            startDestination = Route.UnitsScreen
+        ) {
+            composable<Route.UnitsScreen> {
+                UnitsScreen()
             }
-        )
+        }
     }
 }
